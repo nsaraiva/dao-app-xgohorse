cb3327043ef16d151ffd8da4308ad82a523e2908
Nelson Saraiva <nsaraiva@gmail.com>
2022-08-01 20:18:50 -0300

    Adding wallet connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import{useAddress, useMetamask} from '@thirdweb-dev/react';
 const App = () => {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+
+  console.log("Address:", address);
+
+  if(!address) {
+    return (
+      <div className="landing">
+        <h1>Bem vindo à XGoHorseDAO - A única opção é a mais rápida!</h1>
+        <button onClick={connectWithMetamask}>
+          Conecte sua carteira
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Bem-vind@s à minha DAO</h1>
+      <h1>Bem-vindo DEVs. Resolveu o problema? Compilou? Commit e era isso.</h1>
     </div>
   )
 }
