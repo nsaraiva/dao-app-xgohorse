194a8b9f036fc4e30e8a7e709a23178cc6e9bdfa
Nelson Saraiva <nsaraiva@gmail.com>
2022-08-03 16:02:23 -0300

    Build a button to mint a NFT

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable no-useless-concat */
 import{useAddress, useMetamask, useEditionDrop} from '@thirdweb-dev/react';
 import {useState, useEffect} from 'react';
 
 const App = () => {
+  // Using the hooks thirdweb gives us
   const address = useAddress();
   const connectWithMetamask = useMetamask();
 
@@ -11,6 +13,9 @@ const App = () => {
   const editionDrop = useEditionDrop("0x81d8950aeA472C1d3c39D11379102542e6194491");
   // State variable to know if the user has our NFT.
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+  // isClaiming helps us know if it is in the loading state while the NFT is minted.
+  const [isClaiming,setIsClaiming] = useState(false);
+
 
   useEffect(() => {
     // If he doesn't have a wallet attached, leave!
@@ -37,10 +42,24 @@ const App = () => {
     checkBalance();
   }, [address, editionDrop])
 
+  const mintNft = async () => {
+    try {
+      setIsClaiming(true);
+      await editionDrop.claim("0", 1);
+      console.log(`🌊 Cunhado com sucesso! Olhe na OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`);
+      setHasClaimedNFT(true);
+    } catch (error) {
+        setHasClaimedNFT(false);
+        console.error("Falha ao cunhar NFT", error);
+    } finally {
+      setIsClaiming(false);
+    }
+  };
+
   if(!address) {
     return (
       <div className="landing">
-        <h1>Bem vindo à XGoHorseDAO - A única opção é a mais rápida!</h1>
+        <h1>Bem vindo à XGoHorseDAO - A única opção é a mais rápida! 🚀</h1>
         <button onClick={connectWithMetamask}>
           Conecte sua carteira
         </button>
@@ -48,11 +67,24 @@ const App = () => {
     );
   }
 
+  if (hasClaimedNFT) {
+    return (
+      <div className="landing">
+        <h1>Bem-vindo Dev. Resolveu o problema? Compilou? Commit e era isso. 🏆</h1>
+        {setHasClaimedNFT ? `🌊 Seu NFT na OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0` : ""}
+      </div>
+    )
+  }
+
+  // Render the NFT minting screen
   return (
-    <div className="landing">
-      <h1>Bem-vindo DEVs. Resolveu o problema? Compilou? Commit e era isso.</h1>
+    <div className="mint-nft">
+      <h1>Cunhe gratuitamente seu NFT de membro 🐴 da XGoHorseDAO</h1>
+      <button disable={isClaiming.toString()} onClick={mintNft}>
+        {isClaiming ? "Cunhando..." : "Cunhe seu NFT (GRÁTIS)"} 
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default App
